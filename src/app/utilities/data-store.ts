fe5291100d8c061d03cccef24e60df9a13c0bc0f
ashinkuniyil <ashinkuniyil@gmail.com>
2021-06-29 10:57:14 +0530

    1 file modified

--- src/app/utilities/data-store.ts
+++ src/app/utilities/data-store.ts
@@ -1,40 +1,65 @@
-export class DataStore {
+/**
+ * Data Store main class
+ */
+ export class DataStore {
   private static _instance: DataStore = new DataStore();
 
   applnContext: any = {};
 
+
   constructor() {
-    DataStore._instance = this;
+      if (DataStore._instance) {
+          throw new Error('Error: Instantiation failed: Use DataStore.getInstance() instead of new.');
+      }
+      DataStore._instance = this;
   }
 
+  /**
+   * Returns DataStore instance
+   */
   public static getInstance(): DataStore {
-    return DataStore._instance;
+      return DataStore._instance;
   }
 
   clearAll() {
-    this.applnContext = {};
+      this.applnContext = {};
   }
 
-  setData(contextName: string, contextValue: any) {
-    this.applnContext[contextName] = contextValue;
+
+  /**
+   * Creates new application context data
+   * @param {type} carrierName - creates application context data for provided name.
+   * @param {type} carrierValue - application context value
+   */
+  setData(contextName, contextValue) {
+      this.applnContext[contextName] = contextValue;
   }
 
-  getData(contextName: string) {
-    if (this.applnContext[contextName]) {
-      return this.applnContext[contextName];
-    }
+  /**
+   * Returns data stored in 'contextName'
+   */
+  getData(contextName) {
+      if (this.applnContext[contextName]) {
+          return this.applnContext[contextName];
+      }
   }
 
-  clearContextData(contextName: string) {
-    if (this.applnContext[contextName]) {
-      this.applnContext[contextName] = null;
-    }
+  /**
+   * Clear data stored in 'contextName'
+   */
+  clearContextData(contextName) {
+      if (this.applnContext[contextName]) {
+          this.applnContext[contextName] = null;
+      }
   }
 
-  isContextDataExists(contextName: string) {
-    if (this.applnContext[contextName]) {
-      return true;
-    }
-    return false;
+  /**
+   * Check whether data stored in 'contextName'
+   */
+  isContextDataExists(contextName) {
+      if (this.applnContext[contextName]) {
+          return true;
+      }
+      return false;
   }
 }
